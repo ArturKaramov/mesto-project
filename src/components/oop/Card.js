a979287efba821a9859226bf6aff401ffe33fa25
Artur Karamov <karamov.1311@yandex.ru>
2023-01-20 23:16:51 +0300

    add Card, Section and UserInfo

--- src/components/oop/Card.js
+++ src/components/oop/Card.js
@@ -14,3 +14,60 @@
 // свяжите класс Card c попапом. Сделайте так, чтобы Card принимал в конструктор функцию handleCardClick.
 // При клике на карточку эта функция должна открывать попап с картинкой.
 
+export default class Card {
+  constructor({name, link, likes, owner, _id}, {deleteCallback, likeCallback, handleCardClick}, selector, userId) {
+    this.name = name;
+    this.link = link;
+    this.likes = likes;
+    this.id = owner._id;
+    this.cardId = _id;
+    this._deleteCallback = deleteCallback;
+    this._likeCallback = likeCallback;
+    this._handleCardClick = handleCardClick;
+    this.selector = selector;
+    this.userId = userId;
+  }
+
+  _getElement() {
+    const cardElement = document
+      .querySelector(this.selector)
+      .content
+      .querySelector('.element')
+      .cloneNode(true)
+    return cardElement
+  }
+
+  _createCard() {
+    this._element = this._getElement();
+    this._element.dataset.id = this.cardId;
+    this._element.querySelector('.element__photo').src = this.link;
+    this._element.querySelector('.element__photo').alt = this.name;
+    this._element.querySelector('.element__name').textContent = this.name;
+    this._element.querySelector('.element__likes-number').textContent = this.likes.length;
+    if (this.likes.some((like) => {return like._id === this.userId})) {
+      this._element.querySelector('.element__like').classList.add('element__like_active')
+    };
+    this._setEventListeners();
+    if (this.id !== this.userId) {this._element.querySelector('.element__delete').remove()}
+    return this._element
+  }
+
+  _setEventListeners() {
+    this._element.querySelector('.element__delete').addEventListener('click', (evt) => {this._deleteCallback(evt)});
+    this._element.querySelector('.element__like').addEventListener('click', (evt) => {this._likeCallback(evt)});
+    this._element.querySelector('.element__photo').addEventListener('click', () => {this._handleCardClick(this.name, this.link)});
+  }
+
+  getCard() {
+    return this._createCard()
+  }
+};
+
+const changeLikeCondition = (card, likesNum) => {
+  const cardLikesNum = card.querySelector('.element__likes-number');
+  const cardLikeButton = card.querySelector('.element__like');
+  cardLikesNum.textContent = likesNum;
+  cardLikeButton.classList.toggle('element__like_active');
+};
+
+export { changeLikeCondition }
--- src/components/oop/UserInfo.js
+++ src/components/oop/UserInfo.js
@@ -7,3 +7,20 @@
 // Когда данные пользователя нужно будет подставить в форму при открытии — метод вам пригодится.
 // Содержит публичный метод setUserInfo, который принимает новые данные пользователя, отправляет их на сервер и добавляет их на страницу.
 
+export default class UserInfo {
+  constructor({nameSelector, aboutSelector}) {
+    this._nameNode = document.querySelector(nameSelector);
+    this._aboutNode = document.querySelector(aboutSelector);
+  }
+
+  getUserInfo() {
+    return {name: this._name, about: this._about}
+  }
+
+  setUserInfo({name, about}) {
+    this._name = name;
+    this._about = about;
+    this._nameNode.textContent = this._name;
+    this._aboutNode.textContent = this._about;
+  }
+}
