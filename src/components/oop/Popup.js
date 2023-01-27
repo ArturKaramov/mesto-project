4958bdc6ba844c5280bbfce0646e763179c20b2f
Alexander Morugin <nobilis@bk.ru>
2023-01-28 00:17:22 +0300

    fix Popup after review 1

--- src/components/oop/Popup.js
+++ src/components/oop/Popup.js
@@ -11,21 +11,25 @@ export default class Popup {
   constructor(selector) {
     this.selector = selector;
     this._popup = document.querySelector(selector);
+
+    this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   open() {
     this._popup.classList.add('popup_opened');
+    document.addEventListener('keydown', this._handleEscClose);
   }
 
   close() {
     this._popup.classList.remove('popup_opened');
+    document.removeEventListener('keydown', this._handleEscClose);
   }
 
   _handleEscClose(evt) {
     if (evt.key === 'Escape') {
       this.close();
     }
-  };
+  }
 
   _handleOverlayClose(evt) {
     if (evt.target === evt.currentTarget) {
@@ -43,7 +47,7 @@ export default class Popup {
 
   setEventListeners() {
     this._popup.querySelector('.popup__close').addEventListener('click', () => this.close());
-    document.addEventListener('keydown', (evt) => this._handleEscClose(evt));
+    // document.addEventListener('keydown', (evt) => this._handleEscClose(evt));
     this._popup.addEventListener('mousedown', (evt) => this._handleOverlayClose(evt));
   }
 }
